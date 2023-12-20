183cef00abb17ec5e2b48eeea6678783a7bab804
bleand <baslaleandro@gmail.com>
2023-12-20 00:57:11 -0300

    Different data structure

--- satisfactory-calculator.js
+++ satisfactory-calculator.js
@@ -32,9 +32,9 @@ async function loadRecipesData() {
       levelsData = {};
       // rawMaterials = [];
       // rawMaterialsList = [];
-      !(1 in levelsData) && (levelsData[1] = []);
-      levelsData[1] = selectedElement;
-      expandRecipe(selectedElement);
+    //   !(1 in levelsData) && (levelsData[1] = []);
+      levelsData[selectedElement] = {"quantity":1,"raw":false, "elements":[]};
+      levelsData = expandRecipe(levelsData, selectedElement);
       console.log(levelsData);
     });
   } catch (error) {
@@ -57,22 +57,32 @@ loadRecipesData();
 //   }
 
 // Function to expand recipe and calculate raw materials
-function expandRecipe(element, quantity = 1, level = 2) {
+function expandRecipe(data, element, quantity = 1, level = 2) {
   // const ingredientList = document.getElementById("ingredients-list");
   // const rawMaterialsList = document.getElementById("raw-materials-list");
 
 //   ingredients = parseRecipe(element);
 
   if (recipesData[element]) {
-    !(level in levelsData) && (levelsData[level] = []);
+    // !(level in levelsData) && (levelsData[level] = []);
     const ingredients = recipesData[element]["recipe"];
-    levelsData[level] = levelsData[level].concat(ingredients);
-
+    // levelsData[level] = levelsData[level].concat(ingredients);
+    
     for (const ingredient of ingredients) {
+        console.log(level);
         console.log(ingredient);
+        // console.log(ingredient);
+        // console.log(data);
         const childElement = ingredient.name;
         const childQuantity = ingredient.quantity;
-        expandRecipe(childElement, childQuantity, level + 1);
+        const childData = {};
+        childData[childElement] = {"quantity":childQuantity,"raw":false, "elements":[]};
+        // data[element]["elements"] = data[element]["elements"].concat(expandRecipe(childData, childElement, childQuantity, level + 1)[childElement].elements);
+        data[element]["elements"][childElement] = expandRecipe(childData, childElement, childQuantity, level + 1);
+        // console.log(data[element]["elements"]);
+        // console.log(level);
+        console.log(level);
+        console.log(ingredients);
         // if (recipes[childElement]) {
         //   // ingredientList.innerHTML += `<li>Level ${level}: ${ingredient.quantity} ${childElement}</li>`;
         //   // expandRecipe(childElement, level + 1);
@@ -82,18 +92,20 @@ function expandRecipe(element, quantity = 1, level = 2) {
         // }
     
         // rawMaterialsList.innerHTML = rawMaterials.join("<br>");
+        
       }
-
+      return data;
   } else {
     // levelsData[level] = levelsData[level].concat(element);
-    return false;
+    data[element]["raw"] = true;
+    return data;
   }
 
   // ingredients = parseRecipe(element);
 
   // levelsData[level] = levelsData[level].concat(ingredients);
 
-  console.log(levelsData);
+//   console.log(levelsData);
 
   // ingredientList.innerHTML = "";
   // rawMaterials = [];
